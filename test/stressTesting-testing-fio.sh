867d0cff65361be724f1124ecdc992d09912d326
mr-j-1992 <785303107@qq.com>
2024-09-11 16:04:24 +0800

    Create stressTesting-testing-fio.sh

---
+++ test/stressTesting-testing-fio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt update
+apt install fio -y
+
+sleep 5
+nohup fio --name=big-file-rand-read --directory=/mnt/data0 --rw=randread --refill_buffers --size=3T --filename=randread.bin > /tmp/public_services_jobs/test/fionvme0.log  2>&1 &
+sleep 5
+nohup fio --name=big-file-rand-read --directory=/mnt/data1 --rw=randread --refill_buffers --size=3T --filename=randread.bin > /tmp/public_services_jobs/test/fionvme1.log  2>&1 &  
