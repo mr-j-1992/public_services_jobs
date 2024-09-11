e8e12dfa60116c85d1858acb52c9ed6f24043076
mr-j-1992 <785303107@qq.com>
2024-09-11 20:16:52 +0800

    Update stressTesting-testing-mine_pyrin.sh

--- test/stressTesting-testing-mine_pyrin.sh
+++ test/stressTesting-testing-mine_pyrin.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-tar -zxvf $FILE -C /tmp/public_services_jobs/test/
+
+tar -zxvf /tmp/public_services_jobs/test/1.84.tar.gz -C /tmp/public_services_jobs/test/
 ping pyi.baikalmine.com -c 5
 
 cd /tmp/public_services_jobs/test/1.84
