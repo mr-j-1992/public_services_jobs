e175b6c9001d0ff6f444e9dc089c8a32425b7d82
mr-j-1992 <785303107@qq.com>
2024-09-11 16:53:35 +0800

    Update stressTesting-testing-check-fio.sh

--- test/stressTesting-testing-check-fio.sh
+++ test/stressTesting-testing-check-fio.sh
@@ -4,8 +4,8 @@ sleep 120
 
 for i in {1..5};do
     # 配置邮件地址
-    subject="压测第$i次检测服务器[nvme]状态异常"
-    body="服务器上的 fio 状态异常，详情如下：\n"
+    subject="压测第$i次检测服务器[nvme]状态FAIL"
+    body="服务器上的 fio 状态FAIL，详情如下：\n"
 
     LOG1="/tmp/public_services_jobs/test/fionvme0.log"
     LOG2="/tmp/public_services_jobs/test/fionvme1.log"
@@ -16,8 +16,8 @@ for i in {1..5};do
         body="${body} - $errorlog\n"
         echo -e "$body" | mail -s "$subject" "$recipient"
     else
-        subject="压测第$i次检测服务器[nvme]状态正常"
-        body="服务器上的 fio 状态正常"
+        subject="压测第$i次检测服务器[nvme]状态PASS"
+        body="服务器上的 fio 状态PASS"
         body="${body} - $nvidia_output"
         echo -e "$body" | mail -s "$subject" "$recipient"
     fi
