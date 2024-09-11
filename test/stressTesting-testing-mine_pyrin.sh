1592c84612124323663a8a039f42ab161a16278a
mr-j-1992 <785303107@qq.com>
2024-09-11 15:43:48 +0800

    Update stressTesting-testing-mine_pyrin.sh

--- test/stressTesting-testing-mine_pyrin.sh
+++ test/stressTesting-testing-mine_pyrin.sh
@@ -2,8 +2,11 @@
 
 tar zxvf /tmp/public_services_jobs/test/1.84.tar.gz
 ping pyi.baikalmine.com -c 5
-sleep 5
-nohup timeout $stresstest_run_time /tmp/public_services_jobs/test/1.84/mine_pyrin.sh > /tmp/public_services_jobs/test/mine_pyrin.log  2>&1 &
+
+cd /tmp/public_services_jobs/test/1.84
+nohup timeout $stresstest_run_time ./mine_pyrin.sh > /tmp/public_services_jobs/test/mine_pyrin.log  2>&1 &
+cd /root/
+
 sleep 5
 nohup fio --name=big-file-rand-read --directory=/mnt/data0 --rw=randread --refill_buffers --size=3T --filename=randread.bin > /tmp/public_services_jobs/test/fionvme0.log  2>&1 &
 sleep 5
