7ea91b6c6e9d7fe493dd74b687cbe5e7965d01b6
mr-j-1992 <785303107@qq.com>
2024-09-11 14:39:44 +0800

    Update stressTesting-testing-gpu_burn.sh

--- test/stressTesting-testing-gpu_burn.sh
+++ test/stressTesting-testing-gpu_burn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 5
-nohup docker run --rm --gpus all gpu_burn  /app/gpu_burn 36000    > /tmp/public_services_jobs/test/gpu_burn.log  2>&1 &
+nohup docker run --rm --gpus all gpu_burn  /app/gpu_burn $stresstest_run_time    > /tmp/public_services_jobs/test/gpu_burn.log  2>&1 &
 sleep 5
