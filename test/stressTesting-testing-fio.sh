ca0e009c68f2749b48810340eb1c6c810c5f1f6e
mr-j-1992 <785303107@qq.com>
2024-09-12 13:02:43 +0800

    Update stressTesting-testing-fio.sh

--- test/stressTesting-testing-fio.sh
+++ test/stressTesting-testing-fio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-nohup fio --name=big-file-rand-read --directory=/mnt/data0 --rw=randread --refill_buffers --size=3T --filename=randread.bin  --time_based=1  --runtime=$stresstest_run_time > /tmp/public_services_jobs/test/fionvme0.log  2>&1 &
+fio --name=big-file-rand-read --directory=/mnt/data0 --rw=randread --refill_buffers --size=3T --filename=randread.bin  --time_based=1  --runtime=$stresstest_run_time > /tmp/public_services_jobs/test/fionvme0.log  2>&1 &
 sleep 5
-nohup fio --name=big-file-rand-read --directory=/mnt/data1 --rw=randread --refill_buffers --size=3T --filename=randread.bin  --time_based=1  --runtime=$stresstest_run_time > /tmp/public_services_jobs/test/fionvme1.log  2>&1 &  
+fio --name=big-file-rand-read --directory=/mnt/data1 --rw=randread --refill_buffers --size=3T --filename=randread.bin  --time_based=1  --runtime=$stresstest_run_time > /tmp/public_services_jobs/test/fionvme1.log  2>&1 &  
