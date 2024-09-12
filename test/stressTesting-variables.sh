25f9cd8358348d15c6d65628dec13f9faae04e1c
mr-j-1992 <785303107@qq.com>
2024-09-12 14:18:28 +0800

    Update stressTesting-variables.sh

--- test/stressTesting-variables.sh
+++ test/stressTesting-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export recipient="lvjiang@dayudpu.com"
+export recipient="lvjiang@dayudpu.com,xuebaoku@dayudpu.com,wangjiayu@dayudpu.com"
 export expected_gpu_count=8
 #export stresstest_run_time=43200
 export stresstest_run_time=3600
