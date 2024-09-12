c844393e5375a0f1dddb0649b4d581a54b9b3e79
mr-j-1992 <785303107@qq.com>
2024-09-12 17:05:02 +0800

    Create stressTesting-before-ping.sh

---
+++ test/stressTesting-before-ping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+subject="!!!服务器ping网关丢包，需要手工排查是否有问题!!!"
+body="服务器ping电信网关100个包有丢包，详情如下：\n"
+
+# 执行 ping 命令，并捕获输出结果
+ping_output=$(ping 180.119.35.225 -c 100)
+
+# 检查丢包情况
+packet_loss=$(echo "$ping_output" | grep -oP '\d+(?=% packet loss)')
+
+# 根据丢包率输出结果
+if [ "$packet_loss" -eq 0 ]; then
+    subject="服务器ping电信网关100个包没有丢包"
+    body="服务器ping电信网关100个包没有丢包，详情如下：\n"
+    body="${body} - $ping_output"
+    echo -e "$body" | mail -s "$subject" "$recipient"
+else
+    body="${body} - $ping_output"
+    echo -e "$body" | mail -s "$subject" "$recipient"
+fi
