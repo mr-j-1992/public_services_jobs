e28b154f9891afd5d11a0f8ce9f6b492d8e0239e
mr-j-1992 <785303107@qq.com>
2024-09-12 10:52:25 +0800

    Delete test/dcgm_exporter.sh

--- test/dcgm_exporter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /tmp/public_services_jobs/test
-tar -zxvf dcgm-exporter-3.3.6-3.4.2.tar.gz
-cd dcgm-exporter-3.3.6-3.4.2
-make binary
-./cmd/dcgm-exporter/dcgm-exporter -f ./etc/default-counters.csv
