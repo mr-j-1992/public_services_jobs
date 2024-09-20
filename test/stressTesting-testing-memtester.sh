7399a7aa95513a61604181baef7678a27ee64d93
mr-j-1992 <785303107@qq.com>
2024-09-20 17:28:08 +0800

    Update stressTesting-testing-memtester.sh

--- test/stressTesting-testing-memtester.sh
+++ test/stressTesting-testing-memtester.sh
@@ -2,10 +2,10 @@
 
 apt install memtester -y
 sleep 5
-nohup  memtester 122880 1  > /tmp/public_services_jobs/test/memtester0.log  2>&1 &
+nohup  memtester 122880 1  > /root/stress/memtester0.log  2>&1 &
 sleep 5
-nohup  memtester 122880 1  > /tmp/public_services_jobs/test/memtester1.log  2>&1 &
+nohup  memtester 122880 1  > /root/stress/memtester1.log  2>&1 &
 sleep 5
-nohup  memtester 122880 1  > /tmp/public_services_jobs/test/memtester2.log  2>&1 &
+nohup  memtester 122880 1  > /root/stress/memtester2.log  2>&1 &
 sleep 5
-nohup  memtester 122880 1  > /tmp/public_services_jobs/test/memtester3.log  2>&1 &
+nohup  memtester 122880 1  > /root/stress/memtester3.log  2>&1 &
