1397163eb59898f2df7e59ed0e21e90b917272e9
mr-j-1992 <785303107@qq.com>
2024-09-20 17:37:31 +0800

    Update stressTesting-testing-memtester.sh

--- test/stressTesting-testing-memtester.sh
+++ test/stressTesting-testing-memtester.sh
@@ -2,10 +2,12 @@
 
 apt install memtester -y
 sleep 5
-nohup  memtester 122880 1  > /root/stress/memtester0.log  2>&1 &
-sleep 5
-nohup  memtester 122880 1  > /root/stress/memtester1.log  2>&1 &
-sleep 5
-nohup  memtester 122880 1  > /root/stress/memtester2.log  2>&1 &
-sleep 5
-nohup  memtester 122880 1  > /root/stress/memtester3.log  2>&1 &
+
+# 定义循环次数
+count=20
+
+for i in $(seq 0 $((count - 1)))
+do
+    nohup memtester $((491520 / count)) 1 > /root/stress/memtester$i.log 2>&1 &
+    sleep 5
+done
