a9ace5355e2cd887ee3ee999ed377fa514645c08
mr-j-1992 <785303107@qq.com>
2024-09-24 16:18:17 +0800

    Create stressTesting-testing-check-memtest.sh

---
+++ test/stressTesting-testing-check-memtest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sleep 120
+
+for i in {1..2};do
+    subject="!!!压测第$i次检测服务器[内存]状态FAIL!!!"
+    body="服务器上的 内存 状态FAIL，详情如下：\n"
+
+    # 检查总内存
+    total_mem=$(free -g | awk '/^Mem:/{print $2}')
+    # 检查总内存是否小于 500G
+    if [ "$total_mem" -lt 500 ]; then
+        body="${body} - 检测到的 内存 数量小于 500G，实际数量为: $total_mem\n"
+    fi
+
+    # 检查日志文件中是否有 'fail' 或 'err'
+    log_files="/root/stress/memtester*.log"    
+    if grep -q -E 'fail|err' $log_files; then
+        errors=$(grep -i  -E 'fail|err' $log_files)
+        body="${body} - 检测到错误信息: $errors\n"
+    fi
+
+    # 如果 body 发生了变化，说明有FAIL，发送邮件
+    if [ "$body" != "服务器上的 内存 状态FAIL，详情如下：\n" ]; then
+        echo -e "$body" | mail -s "$subject" "$recipient"
+    else
+        subject="压测第$i次检测服务器[内存]状态PASS"
+        body="服务器上的内存状态PASS，详情如下：\n"
+        echo -e "$body" | mail -s "$subject" "$recipient"
+    fi
+    sleep  5400
+done
