abeb98ab83a2e3fbd968887b91f24c57b800f390
mr-j-1992 <785303107@qq.com>
2024-09-24 17:57:40 +0800

    Update stressTesting-variables.sh

--- test/stressTesting-variables.sh
+++ test/stressTesting-variables.sh
@@ -2,6 +2,9 @@
 
 export recipient="lvjiang@dayudpu.com,xuebaoku@dayudpu.com,wangjiayu@dayudpu.com"
 export expected_gpu_count=8
+#GPU nvme(fio)时间
 export stresstest_run_time=72000
+#dcgmi -r 时间 0.5 小时
+#mem测试时间 4.5小时
 #export stresstest_run_time=43200
 
