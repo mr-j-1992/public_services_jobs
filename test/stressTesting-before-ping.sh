7acb682301a53be381d2e2bf370666a9e32e955d
mr-j-1992 <785303107@qq.com>
2024-10-28 11:04:56 +0800

    Update stressTesting-before-ping.sh

--- test/stressTesting-before-ping.sh
+++ test/stressTesting-before-ping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-subject="!!!服务器ping网关丢包，需要确认MAC地址是否冲突!!!"
+subject="!!!服务器 $BMC_IP ping网关丢包，需要确认MAC地址是否冲突!!!"
 body="服务器ping电信网关100个包有丢包，详情如下：\n"
 
 # 执行 ping 命令，并捕获输出结果
@@ -11,7 +11,7 @@ packet_loss=$(echo "$ping_output" | grep -oP '\d+(?=% packet loss)')
 
 # 根据丢包率输出结果
 if [ "$packet_loss" -eq 0 ]; then
-    subject="服务器ping电信网关100个包没有丢包"
+    subject="服务器 $BMC_IP ping电信网关100个包没有丢包"
     body="服务器ping电信网关100个包没有丢包，详情如下：\n"
     body="${body} - $ping_output"
     echo -e "$body" | mail -s "$subject" "$recipient"
